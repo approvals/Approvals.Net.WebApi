197752115bdf5632f8d800fcdcfc2ffe0434ef09
Llewellyn Falco <isidore@setgame.com>
2015-07-25 11:44:42 -0700

    Version 1.0.1 w/ @jaybazuzi

---
+++ ApprovalTests.Asp.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApprovalTests.Asp.WebApi")]
+[assembly: AssemblyProduct("ApprovalTests.Asp.WebApi")]
+
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyVersion("1.0")]     // Don't change this
+[assembly: AssemblyFileVersion("1.0.1")] // Change this
